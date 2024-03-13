28f7f6e2f432286cecb4f2736bb4f664a68db1b4
emirayurc <128045913+emirayurc@users.noreply.github.com>
2024-03-13 12:21:19 +0300

    Update 0_HES.py

--- pages/0_HES.py
+++ pages/0_HES.py
@@ -151,7 +151,7 @@ with tab1:
 
        # Eğilim çizgisini grafiğe ekle
        fig.add_scatter(x=yillik_sayilar['date'], y=p(yillik_sayilar['date']), mode='lines',line=dict(color='red'),name='Eğilim Çizgisi')
-       st.plotly_chart(fig)
+       st.plotly_chart(fig,use_container_width=True)
        container2 = st.container(border=True)
        container2.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 with tab2:
