d677599ac4702d055166ded78a992ebf21346bf1
emirayurc <128045913+emirayurc@users.noreply.github.com>
2024-03-13 14:28:25 +0300

    Update Anasayfa.py

--- Anasayfa.py
+++ Anasayfa.py
@@ -30,7 +30,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 def main():
     # Setting up the layout
-    col1, col2 = st.columns([0.3,0.7], gap="small")
+    col1, col2 = st.columns([0.5,0.5], gap="small")
 
     with col1:
         st.image('https://urclimate.com/wp-content/uploads/2023/12/LOGO_UrClimate_4-1.png', width=130)
